044eb9e21535a6c8a56010f13d4b8df01b450296
unknown <helmiefendi64@gmail.com>
2019-04-04 10:48:15 +0700

    Commit pertama

---
+++ Assets/Scripts/LevelScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelScript : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (collision.CompareTag("Player"))
+        {
+            //Loading level with build index
+            SceneManager.LoadScene(1);
+
+            //Loading level with scene name
+            SceneManager.LoadScene(levelName);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+	private PlayerInventoryDisplay playerInventoryDisplay;
+	private int totalStars = 0;
+    void Start()
+	{
+		playerInventoryDisplay = GetComponent < PlayerInventoryDisplay >
+			();
+	}
+	void OnTriggerEnter2D(Collider2D hit)
+	{
+		if (hit.CompareTag("star"))
+		{
+			totalStars++;
+			playerInventoryDisplay.OnChangeStarTotal(totalStars);
+            Destroy(hit.gameObject);
+          
+		}
+
+	}
+  
+  
+}
